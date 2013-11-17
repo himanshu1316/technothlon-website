db9f874ce6575360fa6eb92eba29d0e4108537a2
Rahul Kadyan <rahulkdn@gmail.com>
2013-11-18 00:17:24 +0530

    Navigation changed

--- content/bind.php
+++ content/bind.php
@@ -23,7 +23,9 @@
  */
 function urlBinding($segments){
     $binds = array(
-        '/^\/(index|index\.php|index\.html|index\.htm)?\/?$/' => array('page', 'landing'),
+        '/^\/?$/' => array('page', 'landing'),
+        '/^\/(index|index\.php|index\.html|index\.htm)\/?$/' => array('page', 'home'),
+        '/^\/(about|about\.php|about\.html|about\.htm)\/?$/' => array('page', 'about'),
         '/^\/(structure|structure\.php|structure\.html|structure\.htm)?\/?$/' => array('page', 'structure')
     );
     foreach($binds as $key => $val){
